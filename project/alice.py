5906990a679fbe5abe0f085a577ef31576d8dddd
bjoukovs <borisjoukov@hotmail.com>
2018-12-21 14:10:06 +0100

    alice

--- project/alice.py
+++ project/alice.py
@@ -20,10 +20,13 @@ def makeQubit(alice, basis, val):
 def main():
     with CQCConnection("Alice") as alice:
         
-        #Number of qubits
-        N = 10
 
-        alice.sendClassical("Eve", N, close_after=True, timeout=10)
+        #Number of bits for extractor
+        n = 10
+        #Number of qubits
+        N = 4*n
+
+        alice.sendClassical("Eve", N, timout=10)
 
         #Random basis: 0 = standard, 1=hadamard
         basis = [random.randint(0,1) for i in range(N)]
@@ -31,12 +34,24 @@ def main():
         #Qubits eigenvalues (measurement outcome)
         qubitvals = [random.randint(0,1) for i in range(N)]
 
-        #Qubits
         qubits = []
+
 
         for i in range(N):
 
             qubit = makeQubit(alice, basis[i], qubitvals[i])
+            qubits.append(qubit)
+
+            #sending qubit to bob
+            alice.sendQubit(qubit, "Eve")
+
+
+        #Receiving Bob measurements
+        bob_basis = []
+
+        for i in range(N):
+            meas = alice.recvClassical(timout=10)
+            measurements.append(int.from_bytes(meas, byteorder='big'))
 
 
 
